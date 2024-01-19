0d3cebb77eeb487201e88bb17d67aebb1fbcbdca
tataw-cl <127490325+tataw-cl@users.noreply.github.com>
2024-01-19 21:29:54 +0100

    updated

--- templates.cpp
+++ templates.cpp
@@ -1,6 +1,33 @@
+//template example using a swapping function
+
 template<typename T>
 void swap(T& a, T& b){
     T temp =  a;
     a = b;
     b= temp;
 }
+
+//class template example using a stack
+template<typename T>
+class stack
+{
+private:
+    std::vector<T> elements;
+public:
+ void push(const T& value){
+    elements.push_back(value);
+ }
+
+
+void pop(){
+elements.pop_back();
+}
+
+T& top(){
+return elements.back();
+}
+
+bool empty() const {
+    return elements.empty();
+}
+};
