76621fa9e082b7a37cee24ef5fdc4d57170134b7
tataw-cl <127490325+tataw-cl@users.noreply.github.com>
2024-01-19 21:50:05 +0100

    update

--- templates.cpp
+++ templates.cpp
@@ -8,6 +8,9 @@ void swap(T& a, T& b){
 }
 
 //class template example using a stack
+#include<iostream>
+#include<vector>
+
 template<typename T>
 class stack
 {
@@ -31,3 +34,30 @@ bool empty() const {
     return elements.empty();
 }
 };
+
+//Runner code to run the stack template program
+
+
+int main(){
+    //create a stack opf integers
+    stack<int> intstack;
+
+    //push some values into the stack
+    intstack.push(10);
+    intstack.push(20);
+    intstack.push(30);
+
+    //pop and print the top element
+    std::cout << "Top element: " << intstack.top() << std::endl;
+    intstack.pop();
+
+    //check if stack is empty
+
+    if(intstack.empty()){
+        std::cout << "Stack is empty." << std::endl;
+    }
+    else{
+        std::cout << "Stack is not empty" << std::endl;
+    }
+    return 0;
+}
